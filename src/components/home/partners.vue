3cfc5e1d7b2637b4c2367cedbe9b7222479c456c
Gianni Forlastro <gianni.forlastro@gmail.com>
2020-04-06 17:49:49 +0200

    modifiche grafiche

--- src/components/home/partners.vue
+++ src/components/home/partners.vue
@@ -10,7 +10,6 @@
             <v-flex xs12 md12 class="mt-2">
                 <p class="google-font my-2" style="font-size:110%">General Partners</p>
                 
-
                 <v-layout row wrap class="transparent">
                     <v-flex xs12 sm7 class="elevation-0 transparent">
                         <v-card class="elevation-0 pa-0 ma-0 transparent">
@@ -51,10 +50,8 @@
                                             <span class="google-font">{{itemp.name}}</span>
                                             </v-tooltip>
                                         </a>
-                                        
                                     </v-card>
                                     </v-flex>
-
                                 </v-layout>
                             </v-container>
                         </v-card>
@@ -64,7 +61,7 @@
             </v-flex>
 
             <v-flex xs12 md12 class="mt-3">
-                <p class="google-font my-2" style="font-size:110%">Template Partner</p>
+                <p class="google-font my-2" style="font-size:110%">Companies Partner</p>
 
                 <v-layout class="transparent" >
                     <v-flex xs12 sm11 class="elevation-0 transparent">
@@ -79,12 +76,12 @@
                                         class="white elevation-0"
                                     >
                                     <v-card flat tile class="d-flex pa-2 ma-1 elevation-1" style="border-radius:5px;border-width: 1px; border-style: solid;">
-                                        <a href="https://gdgjalandhar.com" target="_blank">
+                                        <a href="https://synesthesia.it/" target="_blank">
                                         <v-tooltip bottom>
                                             <template v-slot:activator="{ on }">
                                         <v-img
-                                            :src="require('@/assets/img/partners/gdgjalandhar.png')"
-                                            :lazy-src="require('@/assets/img/partners/gdgjalandhar.png')"
+                                            :src="require('@/assets/img/partners/synesthesia.png')"
+                                            :lazy-src="require('@/assets/img/partners/synesthesia.png')"
                                             aspect-ratio="3.0"
                                             class="white"
                                             v-on="on"
@@ -100,15 +97,52 @@
                                             </v-layout>
                                         </v-img>
                                         </template>
-                                        <span class="google-font">GDG Jalandhar</span>
+                                        <span class="google-font">Synesthesia srl</span>
                                         </v-tooltip>
                                         </a>
                                     </v-card>
                                     </v-flex>
-
                                 </v-layout>
+                                <v-layout row wrap class="ma-0 pa-0">
+                                    <v-flex
+                                        xs6
+                                        md2
+                                        sm4
+                                        d-flex
+                                        class="white elevation-0"
+                                    >
+                                    <v-card flat tile class="d-flex pa-2 ma-1 elevation-1" style="border-radius:5px;border-width: 1px; border-style: solid;">
+                                        <a href="https://corley.it/" target="_blank">
+                                        <v-tooltip bottom>
+                                            <template v-slot:activator="{ on }">
+                                        <v-img
+                                            :src="require('@/assets/img/partners/corley.png')"
+                                            :lazy-src="require('@/assets/img/partners/corley.png')"
+                                            aspect-ratio="3.0"
+                                            class="white"
+                                            v-on="on"
+                                        >
+                                            <v-layout
+                                                slot="placeholder"
+                                                fill-height
+                                                align-center
+                                                justify-center
+                                                ma-0
+                                            >
+                                                <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                                            </v-layout>
+                                        </v-img>
+                                        </template>
+                                        <span class="google-font">Corley srl</span>
+                                        </v-tooltip>
+                                        </a>
+                                    </v-card>
+                                    </v-flex>
+                                </v-layout>
+                                
                             </v-container>
                         </v-card>
+                        
                     </v-flex>
                 </v-layout>
 
